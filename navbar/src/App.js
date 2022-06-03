5dc9cf2de89d9438fb570fc4a1bd0f10aef92502
Herdoy Almamun <88125953+herdoycode@users.noreply.github.com>
2022-06-03 08:48:09 +0600

    Add files via upload

---
+++ navbar/src/App.js
@@ -0,0 +1,12 @@
+import Navbar from "./components/Navbar";
+
+const App = () => {
+  return (
+    <>
+      <Navbar />
+      <div className="hero"></div>
+    </>
+  );
+};
+
+export default App;
---
+++ navbar/src/data/navbars.js
@@ -0,0 +1,11 @@
+const topNav = [
+  { id: 1, label: "Home", href: "#" },
+  { id: 2, label: "About", href: "#" },
+  { id: 3, label: "Skills", href: "#" },
+  { id: 5, label: "Portfolio", href: "#" },
+  { id: 5, label: "Contact", href: "#" },
+];
+
+export const getTopNav = () => {
+  return topNav;
+};
